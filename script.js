2605a324fa2e162168cf4430a9beb0339365c1a4
SurendharHi5 <142418478+SurendharHi5@users.noreply.github.com>
2023-10-31 08:50:44 -0800

    script.js

--- script.js
+++ script.js
@@ -1,71 +1,39 @@
-let count = 11;
-setTimeout(() =>{
-    count--;
-    console.log(count);
-    var screen = document.querySelector('#countdown');
-    screen.innerText = count;
-    setTimeout(() =>{
-        count--;
-        console.log(count);
-        var screen = document.querySelector('#countdown');
-        screen.innerText = count;
-        setTimeout(() =>{
-            count--;
-            console.log(count);
-            var screen = document.querySelector('#countdown');
-            screen.innerText = count;
-            setTimeout(() =>{
-                count--;
-                console.log(count);
-                var screen = document.querySelector('#countdown');
-                screen.innerText = count;
-                setTimeout(() =>{
-                    count--;
-                    console.log(count);
-                    var screen = document.querySelector('#countdown');
-                    screen.innerText = count;
-                    setTimeout(() =>{
-                        count--;
-                        console.log(count);
-                        var screen = document.querySelector('#countdown');
-                        screen.innerText = count;
-                        setTimeout(() =>{
-                            count--;
-                            console.log(count);
-                            var screen = document.querySelector('#countdown');
-                            screen.innerText = count;
-                            setTimeout(() =>{
-                                count--;
-                                console.log(count);
-                                var screen = document.querySelector('#countdown');
-                                screen.innerText = count;
-                                setTimeout(() =>{
-                                    count--;
-                                    console.log(count);
-                                    var screen = document.querySelector('#countdown');
-                                    screen.innerText = count;
-                                    setTimeout(() =>{
-                                        count--;
-                                        console.log(count);
-                                        var screen = document.querySelector('#countdown');
-                                        screen.innerText = count;
-                                        setTimeout(() =>{
-                                            count--;
-                                            console.log(count);
-                                            var screen = document.querySelector('#countdown');
-                                            screen.innerText = count;
-                                            clearTimeout();
-                                            console.log("Happy Independence Day");
-                                            var screen = document.querySelector('#countdown');
-                                            screen.innerText = "Happy Independence Day!!!"
-                                        },1000)
-                                    },1000)
-                                },1000)
-                            },1000)
-                        },1000)
-                    },1000)
-                },1000)
-            },1000)
-        },1000)
-    },1000)
-},1000)
+function screen(txt){
+    document.getElementById("countdown").innerText=txt;
+}
+
+function countdown(){
+    screen(10);
+    setTimeout(() => {
+        screen(9);
+        setTimeout(() => {
+            screen(8);
+            setTimeout(() => {
+                screen(7);
+                setTimeout(() => {
+                    screen(6);
+                    setTimeout(() => {
+                        screen(5);
+                        setTimeout(() => {
+                            screen(4);
+                            setTimeout(() => {
+                                screen(3);
+                                setTimeout(() => {
+                                    screen(2);
+                                    setTimeout(() => {
+                                        screen(1);
+                                        setTimeout(() => {
+                                            screen("Happy Independence Day")
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+    
+}
+countdown();
